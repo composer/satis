693cca14a0a5527be6f5cfca9f64cab305e28b1c
Jordi Boggiano <j.boggiano@seld.be>
2012-06-29 11:33:25 +0200

    Update vendors

--- src/Composer/Satis/Console/Application.php
+++ src/Composer/Satis/Console/Application.php
@@ -49,11 +49,11 @@ class Application extends BaseApplication
     /**
      * @return Composer
      */
-    public function getComposer($required = true, $filename = null)
+    public function getComposer($required = true, $config = null)
     {
         if (null === $this->composer) {
             try {
-                $this->composer = Factory::create($this->io, $filename);
+                $this->composer = Factory::create($this->io, $config);
             } catch (\InvalidArgumentException $e) {
                 $this->io->write($e->getMessage());
                 exit(1);
