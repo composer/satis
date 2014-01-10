4b5f4b082e96f5679f25ce5c4d96a53a4b75a781
Josef Kufner <jk@frozen-doe.net>
2014-01-10 17:21:13 +0100

    Load repository configuration from config file and change option name
    Option is now --repository-url, because it better describes expected
    value.
    
    If repository URL is not found in configuration, an exception is thrown,
    to ensure consistency of partial builds with full build.

--- src/Composer/Satis/Command/BuildCommand.php
+++ src/Composer/Satis/Command/BuildCommand.php
@@ -50,7 +50,7 @@ class BuildCommand extends Command
                 new InputArgument('output-dir', InputArgument::OPTIONAL, 'Location where to output built files', null),
                 new InputOption('no-html-output', null, InputOption::VALUE_NONE, 'Turn off HTML view'),
                 new InputOption('skip-errors', null, InputOption::VALUE_NONE, 'Skip Download or Archive errors'),
-                new InputOption('repository', null, InputOption::VALUE_OPTIONAL, 'Only update the given repository', null),
+                new InputOption('repository-url', null, InputOption::VALUE_OPTIONAL, 'Only update the repository at given url', null),
             ))
             ->setHelp(<<<EOT
 The <info>build</info> command reads the given json file
@@ -130,18 +130,32 @@ EOT
         }
 
 
-        if ($input->getOption('repository') !== null) {
-            $singleRepository = $input->getOption('repository');
+        if (($singleRepositoryUrl = $input->getOption('repository-url')) !== null
+            && FALSE !== ($otherPackagesCache = @file_get_contents($outputDir.'/packages.cache')))
+        {
+            $singleRepositoryUrl = $input->getOption('repository-url');
 
-            $otherPackages = unserialize(file_get_contents($outputDir.'/packages.cache'));
-            unset($config['repositories']);
-            $config['repositories'] = array(
-                array('type' => 'vcs', 'url' => $singleRepository),
-            );
+            $otherPackages = unserialize($otherPackagesCache);
+
+            // find repository configuration
+            $singleRepositoryConfig = null;
+            foreach ($config['repositories'] as $r) {
+                if ($r['url'] == $singleRepositoryUrl) {
+                    $singleRepositoryConfig = $r;
+                    break;
+                }
+            }
+            if ($singleRepositoryConfig === null) {
+                throw new \InvalidArgumentException('Requested repository not found in configuration: '.$singleRepositoryUrl);
+            }
+
+            // use only selected repository
+            $config['repositories'] = array($singleRepositoryConfig);
 
             $composer = $this->getApplication()->getComposer(true, $config);
             $packages = $this->selectPackages($composer, $output, $verbose, $requireAll, $requireDependencies);
 
+            // merge with cached data
             $packages = array_merge($otherPackages, $packages);
             ksort($packages, SORT_STRING);
         } else {
