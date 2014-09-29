dfd70eb3e04a08b185576552affaf8c82f9ac7bb
Sergey Kolodyazhnyy <sergey.kolodyazhnyy@gmail.com>
2014-09-30 01:17:46 +0200

    Implement repository URL validation

--- src/Composer/Satis/Command/AddCommand.php
+++ src/Composer/Satis/Command/AddCommand.php
@@ -11,6 +11,9 @@
 
 namespace Composer\Satis\Command;
 
+use Composer\Factory;
+use Composer\IO\NullIO;
+use Composer\Repository\VcsRepository;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
@@ -48,6 +51,8 @@ EOT
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $formatter = $this->getHelperSet()->get('formatter');
+
         $configFile = $input->getArgument('file');
         $repositoryUrl = $input->getArgument('url');
 
@@ -62,16 +67,57 @@ EOT
             return 1;
         }
 
+        if (!$this->isRepositoryValid($repositoryUrl)) {
+            $output->writeln('<error>Invalid Repository URL: '.$repositoryUrl.'</error>');
+            return 3;
+        }
+
         $config = $file->read();
         if (!isset($config['repositories']) || !is_array($config['repositories'])) {
             $config['repositories'] = array();
+        }
+
+        foreach ($config['repositories'] as $repository) {
+            if(isset($repository['url']) && $repository['url'] == $repositoryUrl) {
+                $output->writeln('<error>Repository already added to the file</error>');
+                return 4;
+            }
         }
 
         $config['repositories'][] = array('type' => 'vcs', 'url'  => $repositoryUrl);
 
         $file->write($config);
 
+
+        $output->writeln(array(
+            '',
+            $formatter->formatBlock('Your configuration file successfully updated! It\'s time to rebuild your repository', 'bg=blue;fg=white', true),
+            ''
+        ));
+
         return 0;
+    }
+
+    /**
+     * Validate repository URL
+     *
+     * @param $repositoryUrl
+     * @return bool
+     */
+    protected function isRepositoryValid($repositoryUrl)
+    {
+        $io = new NullIO();
+        $config = Factory::createConfig();
+        $io->loadConfiguration($config);
+        $repository = new VcsRepository(array('url' => $repositoryUrl), $io, $config);
+
+        if (!($driver = $repository->getDriver())) {
+            return false;
+        }
+
+        $information = $driver->getComposerInformation($driver->getRootIdentifier());
+
+        return !empty($information['name']);
     }
 
 }
