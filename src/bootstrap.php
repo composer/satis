551f27a45318a96018a166b8c066120cab0659c9
Rob Bast <rob.bast@gmail.com>
2016-10-07 11:47:51 +0200

    update dockerfile and templates

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -17,10 +17,8 @@ function includeIfExists($file)
 }
 
 if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
-    print('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
-    die(1);
+    print('You must set up the project dependencies using Composer before you can use Satis.');
+    exit(1);
 }
 
 return $loader;
