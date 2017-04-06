c408669462fa7a97af32fd8c920f55a13fc258ee
Vasek Purchart <me@vasekpurchart.cz>
2017-04-06 18:49:49 +0200

    allow to specify Docker entrypoint using cmd parameters

---
+++ bin/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+isCommand() {
+  for cmd in \
+    "add" \
+    "build" \
+    "help" \
+    "init" \
+    "list" \
+    "purge"
+  do
+    if [ -z "${cmd#"$1"}" ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+# check if the first argument passed in looks like a flag
+if [ "$(printf %c "$1")" = '-' ]; then
+  set -- /sbin/tini -- /satis/bin/satis "$@"
+# check if the first argument passed in is satis
+elif [ "$1" = 'satis' ]; then
+  shift
+  set -- /sbin/tini -- /satis/bin/satis "$@"
+# check if the first argument passed in matches a known command
+elif isCommand "$1"; then
+  set -- /sbin/tini -- /satis/bin/satis "$@"
+fi
+
+exec "$@"
