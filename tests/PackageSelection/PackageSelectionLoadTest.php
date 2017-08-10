f66ff72ce4e788e95827404888fd53b3c9d29f82
Tamás Molnár <moltam@gmail.com>
2017-08-10 08:25:40 +0200

    Fix selecting alias instead of package (#432)

--- tests/PackageSelection/PackageSelectionLoadTest.php
+++ tests/PackageSelection/PackageSelectionLoadTest.php
@@ -12,6 +12,7 @@
 namespace Composer\Satis\PackageSelection;
 
 use Composer\Package\Package;
+use Composer\Package\AliasPackage;
 use Composer\Satis\Builder\PackagesBuilder;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamDirectory;
@@ -29,12 +30,25 @@ class PackageSelectionLoadTest extends \PHPUnit_Framework_TestCase
     /** @var Package */
     protected $package;
 
+    /** @var Package */
+    protected $devPackage;
+
     /** @var vfsStreamDirectory */
     protected $root;
 
     protected function setUp()
     {
+        static $extra = [
+            "branch-alias" => [
+                "dev-master" => "1.0-dev",
+            ]
+        ];
+
         $this->package = new Package('vendor/name', '1.0.0.0', '1.0');
+        $this->package->setExtra($extra);
+
+        $this->devPackage = new Package('vendor/name', '9999999-dev', 'dev-master');
+        $this->devPackage->setExtra($extra);
 
         $this->root = $this->setFileSystem();
 
@@ -55,7 +69,7 @@ class PackageSelectionLoadTest extends \PHPUnit_Framework_TestCase
             'repositories' => [['type' => 'composer', 'url' => 'http://localhost:54715']],
             'require' => ['vendor/name' => '*'],
         ], false);
-        $packagesBuilder->dump([$this->package]);
+        $packagesBuilder->dump([$this->package, $this->devPackage]);
 
         return $root;
     }
@@ -104,5 +118,22 @@ class PackageSelectionLoadTest extends \PHPUnit_Framework_TestCase
          */
         $this->selection->setPackagesFilter(['othervendor/othername']);
         $this->assertNotEmpty($this->selection->load());
+    }
+
+    public function testAliasNotSelected()
+    {
+        $this->selection->setPackagesFilter(['othervendor/othername']);
+        $packages = $this->selection->load();
+        $this->assertNotEmpty($packages);
+
+        foreach ($packages as $package) {
+            $this->assertNotInstanceOf(AliasPackage::class, $package);
+
+            if ($package->isDev()) {
+                $this->assertSame('dev-master', $package->getPrettyVersion());
+            } else {
+                $this->assertSame('1.0', $package->getPrettyVersion());
+            }
+        }
     }
 }
