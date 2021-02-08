3da5ca0ca3928a1a6bf677afce573f8b895186b7
Timothy Heimbender <timothy@heimbender.com>
2021-02-07 22:06:20 -0500

    Update npm deps, remove jQuery dependency

--- views/assets/js/app.js
+++ views/assets/js/app.js
@@ -1,4 +1,3 @@
-import $ from 'jquery'
 import 'bootstrap/js/dist/collapse'
 import DateDistance from './App/DateDistance'
 import PackageFilter from './App/PackageFilter'
@@ -7,7 +6,7 @@ function updateTimeElements () {
   DateDistance.calculate('time')
 };
 
-$(function() {
+document.addEventListener("DOMContentLoaded", function() {
   new PackageFilter('input#search', '#package-list', '.card')
   updateTimeElements()
   window.setInterval(updateTimeElements, 5000)
