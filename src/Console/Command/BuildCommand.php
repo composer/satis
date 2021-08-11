8c668d106ca175b91aaf5c155253d091af7399b8
Chris Smith <chris.smith@widerplan.com>
2021-08-11 16:59:38 +0100

    Change `repository-url` option to require a value
    Doesn't make a lot of sense without a value.

--- src/Console/Command/BuildCommand.php
+++ src/Console/Command/BuildCommand.php
@@ -43,7 +43,7 @@ class BuildCommand extends BaseCommand
                 new InputArgument('file', InputArgument::OPTIONAL, 'Json file to use', './satis.json'),
                 new InputArgument('output-dir', InputArgument::OPTIONAL, 'Location where to output built files', null),
                 new InputArgument('packages', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Packages that should be built. If not provided, all packages are built.', null),
-                new InputOption('repository-url', null, InputOption::VALUE_OPTIONAL, 'Only update the repository at given url', null),
+                new InputOption('repository-url', null, InputOption::VALUE_REQUIRED, 'Only update the repository at given url', null),
                 new InputOption('repository-strict', null, InputOption::VALUE_NONE, 'Also apply the repository filter when resolving dependencies'),
                 new InputOption('no-html-output', null, InputOption::VALUE_NONE, 'Turn off HTML view'),
                 new InputOption('skip-errors', null, InputOption::VALUE_NONE, 'Skip Download or Archive errors'),
