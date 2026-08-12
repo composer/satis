22430ec8263065d64cf2ea58addc212d45f7a957
Rob Bast <rob.bast@gmail.com>
2026-08-12 10:18:06 +0200

    update config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,10 +2,15 @@
 export default {
     // arrowParens: 'always',
     // bracketSameLine: false,
+    // bracketSpacing: true,
+    // objectWrap: 'preserve',
     printWidth: 160,
+    // proseWrap: 'preserve',
     // quoteProps: 'as-needed',
     semi: false,
+    // singleAttributePerLine: false,
     singleQuote: true,
     // tabWidth: 2,
     // trailingComma: 'all',
+    // useTabs: false,
 }
